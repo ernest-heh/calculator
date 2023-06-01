6dc9f65c86dfa49ae9b29f47d298042c4a68f1a0
Ernest Ho <ernest.heh@gmail.com>
2023-06-01 13:16:51 +0800

    add ui for calculator

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const calculatorScreen = document.querySelector(".calc-screen");
+
+calculatorScreen.textContent = 12312.2133;
+
+const clearScreen = () => {
+  calculatorScreen.textContent = "";
+};
+
+setTimeout(() => {
+  clearScreen();
+}, 1000);
+
 const add = (a, b) => {
   return a + b;
 };
@@ -12,4 +24,23 @@ const multiply = (a, b) => {
 
 const divide = (a, b) => {
   return a / b;
+};
+
+let num1 = 0;
+let num2 = 0;
+let operator;
+
+const operate = (a, b, operand) => {
+  switch (operand) {
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "*":
+      return a * b;
+    case "/":
+      return a / b;
+    default:
+      return 0;
+  }
 };
