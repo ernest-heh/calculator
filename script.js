7f292af6bcf30c432e121761d80ba7ac05f4377b
Ernest Ho <ernest.heh@gmail.com>
2023-06-01 19:13:34 +0800

    cleanup code

--- script.js
+++ script.js
@@ -68,14 +68,14 @@ const calculate = (a, b, operand) => {
   if (operand === "divide") return a / b;
 };
 
-window.addEventListener("keyup", handleKeyboardInput);
+// window.addEventListener("keyup", handleKeyboardInput);
 
-const handleKeyboardInput = (e) => {
-  if (e.key >= 0 && e.key <= 9) appendNumber(e.key);
-  if (e.key === ".") appendPoint();
-  if (e.key === "=" || e.key === "Enter") evaluate();
-  if (e.key === "Backspace") deleteNumber();
-  if (e.key === "Escape") clearScreen();
-  if (e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/")
-    setOperation(convertOperator(e.key));
-};
+// const handleKeyboardInput = (e) => {
+//   if (e.key >= 0 && e.key <= 9) appendNumber(e.key);
+//   if (e.key === ".") appendPoint();
+//   if (e.key === "=" || e.key === "Enter") evaluate();
+//   if (e.key === "Backspace") deleteNumber();
+//   if (e.key === "Escape") clearScreen();
+//   if (e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/")
+//     setOperation(convertOperator(e.key));
+// };
